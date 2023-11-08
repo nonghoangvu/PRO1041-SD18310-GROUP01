887f6c1f60297474942f11e1f8c20c96c51212d9
vunhph33506 <vunhph33506@fpt.edu.vn>
2023-11-08 12:21:36 +0700

    Create Model Milk

---
+++ UDPM-Java/src/main/java/udpm/fpt/edu/model/Milk.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package udpm.fpt.edu.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author NONG HOANG VU
+ */
+@Entity
+@Table(name = "Milk")
+@Getter
+@Setter
+public class Milk {
+    @Id
+    private Long id;
+    @Column
+    private String product_name;
+    @Column
+    private String img;
+    @Column
+    private Long flavor_id;
+    @Column
+    private Float price_retail;
+    @Column
+    private Float price_wholesale;
+    @Column
+    private int amount;
+    @Column
+    private Date production_date;
+    @Column
+    private Date expiration_date;
+    @Column
+    private String provider;
+    @Column
+    private Date create_at;
+    @Column
+    private String create_by;
+}
