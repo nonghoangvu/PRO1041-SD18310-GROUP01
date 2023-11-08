43f4ba3dcb5479d192699be9b225c7771a82b7c8
vunhph33506 <vunhph33506@fpt.edu.vn>
2023-11-08 12:14:00 +0700

    New config

---
+++ UDPM-Java/src/main/java/udpm/fpt/edu/config/DbMetadata.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package udpm.fpt.edu.config;
+
+public final class DbMetadata {
+    private static final String USERNAME = "sa";
+    private static final String PASSWORD = "123";
+    private static final String SERVER = "localhost";
+    private static final String PORT = "1433";
+    private static final String DATABASE_NAME = "JPA";
+    private static final boolean USING_SSL = true;
+    private static String CONNECT_STRING;
+
+    static {
+        StringBuilder connectStringBuilder = new StringBuilder();
+        connectStringBuilder.append("jdbc:sqlserver://")
+                .append(SERVER).append(":").append(PORT).append(";")
+                .append("databaseName=").append(DATABASE_NAME).append(";")
+                .append("user=").append(USERNAME).append(";")
+                .append("password=").append(PASSWORD).append(";");
+        if (USING_SSL) {
+            connectStringBuilder.append("encrypt=true;trustServerCertificate=true;");
+        }
+        CONNECT_STRING = connectStringBuilder.toString();
+    }
+
+    public static String getConnectString() {
+        System.out.println(CONNECT_STRING);
+        return CONNECT_STRING;
+    }
+}
