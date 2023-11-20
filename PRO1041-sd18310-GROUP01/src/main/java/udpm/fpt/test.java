dc80f5f14c710e715157b4df1b78db59e9b6621d
vunhph33506 <vunhph33506@fpt.edu.vn>
2023-11-20 10:26:02 +0700

    test Location

--- PRO1041-sd18310-GROUP01/src/main/java/udpm/fpt/test.java
+++ PRO1041-sd18310-GROUP01/src/main/java/udpm/fpt/test.java
@@ -12,9 +12,9 @@ public class test {
 
     public static void main(String[] args) {
         test instance = new test();
-        System.out.println(new javax.swing.ImageIcon(instance.getClass().getResource("/udpm/icon/LogoTH.png")));
+//        System.out.println(new javax.swing.ImageIcon(instance.getClass().getResource("/udpm/icon/LogoTH.png")));
 //        System.out.println(new javax.swing.ImageIcon(test.class.getResource("/absolute/path/to/LogoTH.png")));
-        System.out.println(test.class.getResource("/udpm/icon/LogoTH.png"));
+        System.out.println(test.class.getResource("/udpm/fpt/icon/LogoTH.png"));
 
     }
 }
