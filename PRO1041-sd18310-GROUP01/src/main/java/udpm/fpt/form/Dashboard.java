cd9ccee5fc5e33ac1b009bef261d54f987781d9f
vunhph33506 <vunhph33506@fpt.edu.vn>
2023-12-10 14:58:14 +0700

    Update  chart

--- PRO1041-sd18310-GROUP01/src/main/java/udpm/fpt/form/Dashboard.java
+++ PRO1041-sd18310-GROUP01/src/main/java/udpm/fpt/form/Dashboard.java
@@ -1,7 +1,7 @@
 package udpm.fpt.form;
 
 import java.awt.Color;
-import udpm.fpt.swing.Chart.blankchart.charts.ModelChart;
+import udpm.fpt.swing.PolarAreaChartPackage.ModelPolarAreaChart;
 
 /**
  *
@@ -11,57 +11,111 @@ public class Dashboard extends javax.swing.JPanel {
 
     public Dashboard() {
         initComponents();
-        chart.addLegend("A", new Color(245, 189, 135));
-        chart.addLegend("B", new Color(135, 189, 245));
-        chart.addLegend("C", new Color(189, 135, 245));
-        chart.addLegend("D", new Color(139, 229, 222));
-        chart.addData(new ModelChart("1", new double[]{1000, 2, 6, 7}));
-        chart.addData(new ModelChart("2", new double[]{600, 750, 90, 150}));
-        chart.addData(new ModelChart("3", new double[]{200, 350, 460, 900}));
-        chart.addData(new ModelChart("4", new double[]{480, 150, 750, 700}));
-        chart.addData(new ModelChart("5", new double[]{350, 540, 300, 150}));
-        chart.addData(new ModelChart("6", new double[]{190, 280, 81, 200}));
-        chart.start();
+        ProductStatistics();
+        RevenueStatistics();
+    }
+
+    private void ProductStatistics() {
+        productStatistics.addItem(new ModelPolarAreaChart(new Color(52, 148, 203), "Total number of products", 60));
+        productStatistics.addItem(new ModelPolarAreaChart(new Color(175, 67, 237), "Inventory quantity", 50));
+        productStatistics.addItem(new ModelPolarAreaChart(new Color(87, 218, 137), " Expired products", 30));
+        productStatistics.start();
+    }
+    private void RevenueStatistics(){
+        revenueStatistics.addItem(new ModelPolarAreaChart(new Color(52, 148, 203), "Null", 60));
+        revenueStatistics.addItem(new ModelPolarAreaChart(new Color(175, 67, 237), "Null", 50));
+        revenueStatistics.addItem(new ModelPolarAreaChart(new Color(87, 218, 137), "Null", 30));
+        revenueStatistics.start();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        chart = new udpm.fpt.swing.Chart.blankchart.charts.ChartMain();
         jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        productStatistics = new udpm.fpt.swing.PolarAreaChartPackage.PolarAreaChart();
+        jPanel2 = new javax.swing.JPanel();
+        revenueStatistics = new udpm.fpt.swing.PolarAreaChartPackage.PolarAreaChart();
 
         setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/cowGif.gif"))); // NOI18N
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Product statistics", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 1, 14), new java.awt.Color(229, 229, 229))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(productStatistics, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(62, Short.MAX_VALUE)
+                .addComponent(productStatistics, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Revenue statistics", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 1, 14), new java.awt.Color(229, 229, 229))); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(revenueStatistics, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(26, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(revenueStatistics, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(chart, javax.swing.GroupLayout.PREFERRED_SIZE, 691, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(37, 37, 37)
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 377, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 528, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(422, Short.MAX_VALUE))
+                .addGap(77, 77, 77))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(79, 79, 79)
-                        .addComponent(chart, javax.swing.GroupLayout.PREFERRED_SIZE, 502, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap()
+                        .addComponent(jLabel1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(14, 14, 14)
-                        .addComponent(jLabel1)))
-                .addContainerGap(269, Short.MAX_VALUE))
+                        .addGap(19, 19, 19)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(252, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private udpm.fpt.swing.Chart.blankchart.charts.ChartMain chart;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private udpm.fpt.swing.PolarAreaChartPackage.PolarAreaChart productStatistics;
+    private udpm.fpt.swing.PolarAreaChartPackage.PolarAreaChart revenueStatistics;
     // End of variables declaration//GEN-END:variables
 }
